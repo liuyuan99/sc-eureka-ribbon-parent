aa0a3fd52690cc0dee148d83fb5861d94156f3e1
liuyuan <805764579@qq.com>
2020-07-17 08:54:10 +0800

    初始化

---
+++ sc-consumer-hytrix/src/main/java/com/liuyuan/sc/service/UserServiceFailBack.java
@@ -0,0 +1,31 @@
+package com.liuyuan.sc.service;
+
+import com.liuyuan.sc.Tuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceFailBack implements UserService {
+
+    @Override
+    public String getNameById(Integer uid) {
+        return "没有得到【"+uid+"】的用户";
+    }
+
+    @Override
+    public List<String> testlist() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("旺旺碎冰冰");
+        list.add("可口可乐");
+        return list;
+    }
+
+    @Override
+    public List<Tuser> getUserList() {
+        ArrayList<Tuser> tusers = new ArrayList<>();
+        Tuser tuser = new Tuser();
+        tuser.setName("男");
+        tusers.add(tuser);
+        return tusers;
+    }
+}
--- sc-user-entity/src/main/java/com/liuyuan/sc/Tuser.java
+++ sc-user-entity/src/main/java/com/liuyuan/sc/Tuser.java
@@ -36,4 +36,7 @@ public class Tuser implements Serializable {
     private String name;
 
 
+    public Tuser() {
+
+    }
 }
